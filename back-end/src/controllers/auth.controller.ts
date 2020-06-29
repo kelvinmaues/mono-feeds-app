be4c2198b692f05a31fea4869b6acbc3c0834e06
Kelvin Maues <kgmdeveloper@gmail.com>
2020-06-29 12:46:27 -0300

    Signing user up

--- back-end/src/controllers/auth.controller.ts
+++ back-end/src/controllers/auth.controller.ts
@@ -1,7 +1,27 @@
 import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcryptjs";
 import { User } from "../models/user.model";
 
 export const signUp = (req: Request, res: Response, next: NextFunction) => {
   const { email, password, name } = req.body;
 
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPass) => {
+      const user = new User({
+        email,
+        password: hashedPass,
+        name,
+      });
+      return user.save();
+    })
+    .then(() => {
+      res.status(201).json({ success: true });
+    })
+    .catch((err) => {
+      if (!(err.statusCode === 500)) {
+        err.statusCode === 500;
+      }
+      next(err);
+    });
 };
--- back-end/src/models/user.model.ts
+++ back-end/src/models/user.model.ts
@@ -15,7 +15,7 @@ const userSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
-  status: { type: String, required: true },
+  status: { type: String, default: "new" },
   posts: [
     {
       type: Schema.Types.ObjectId,
